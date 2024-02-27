b2a50217802d82493d0ffa96c26942b07435ffa8
geranhuns <gera.nuncio@gmail.com>
2024-02-26 20:22:25 -0600

    added header

--- Banderas/my-project/src/App.jsx
+++ Banderas/my-project/src/App.jsx
@@ -8,7 +8,10 @@ function App() {
 
   return (
     <>
-
+      <header className="flex justify-between">
+        <h1 >Where in the world?</h1>
+        <button >Dark Mode</button>
+      </header >
     </>
   )
 }
